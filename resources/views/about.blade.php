630bbad1aaaf7643406f304e49cb084e606e3fae
Moch. Nizar Al Fattan <96805803+Nizaralfattan@users.noreply.github.com>
2024-01-03 15:32:03 +0700

    Add files via upload

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,99 +6,95 @@
   <div class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-15 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
     <div>
       <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl ">Sweet's Parfume</h2>
-      <p class="mt-4 text-gray-500">Dengan adanya moto tersebut, kami selalu melakukan perbaikan untuk mencapai dan semakin menyempurnakannya. 
+      <p class="mt-4 text-gray-500">Dengan adanya moto tersebut, kami selalu melakukan perbaikan untuk mencapai dan semakin menyempurnakannya.
 
-Untuk keaslian parfum yang kami jual, kita bekerjasama dengan semua distributor resmi parfum original di Indonesia, karena itu kami yakin bahwa parfum yang kami jual adalah 100% Original. 
+        Untuk keaslian parfum yang kami , kita bekerjasama dengan semua distributor resmi parfum original di Indonesia, karena itu kami yakin bahwa parfum yang kami jual adalah 100% Original.
 
-Dalam hal harga kami tentu menjadi Benchmark dari toko online parfum yang lain, karena kami selalu memberikan harga yang terbaik untuk konsumen kami. Untuk harga parfum yang kami tawarkan, kami yakin harga kami MURAH. Kami menggunakan jasa pengiriman ternama untuk mengirim semua pesanan parfum dari customer kami, hal ini untuk menjamin bahwa parfum yang dipesan sampai sesuai dengan skedul (jadwal) yang sudah diinformasikan. Hal ini adalah salah satu usaha kami untuk mencapai moto kami CEPAT. .</p>
+        Dalam hal harga kami tentu menjadi Benchmark dari toko online parfum yang lain, karena kami selalu memberikan harga yang terbaik untuk konsumen kami. Untuk harga parfum yang kami tawarkan, kami yakin harga kami MURAH. Kami menggunakan jasa pengiriman ternama untuk mengirim semua pesanan parfum dari customer kami, hal ini untuk menjamin bahwa parfum yang dipesan sampai sesuai dengan skedul (jadwal) yang sudah diinformasikan. Hal ini adalah salah satu usaha kami untuk mencapai moto kami CEPAT. .</p>
 
 
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Netstars Team</h2>
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Netstars Team</h2>
       <p class="mt-4 text-gray-500">The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder coated steel divider separates active cards from new ones, or can be used to archive important task lists.</p>
 
       </dl>
     </div>
     <div class="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8 block mb-4">
       <a href="/public">
-      <img src="/img/sweets.png" alt="Top down view of walnut card tray with embedded magnets and card groove." class="rounded-lg mb-4">
+        <img src="/img/sweets.png" alt="Top down view of walnut card tray with embedded magnets and card groove." class="rounded-lg mb-4">
       </a>
       <a href="/public">
-      <img src="/img/Netstars.png" alt="." class="rounded-lg mb-4">
+        <img src="/img/Netstars.png" alt="." class="rounded-lg mb-4">
       </a>
     </div>
   </div>
-  
+
 
   <div class="flex justify-center gap-4 mt-10 mb-20">
-      <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img src="">
+
+
+        <img class="rounded-t-lg p-8 " src="https://source.unsplash.com/random/250%C3%97250/?parfume" alt="product image">
+
+      </a>
+      <div class="px-5 pb-5">
         <a href="#">
-          <img src="">
-
-            
-          <img class="rounded-t-lg p-8 " src="https://source.unsplash.com/random/250%C3%97250/?parfume" alt="product image">
-
+          <h3 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Adam Maulana Kusumah</h3>
+          <h5 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Front End Developer</h5>
         </a>
-          <div class="px-5 pb-5">
-            <a href="#">
-              <h3 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Adam Maulana Kusumah</h3>
-              <h5 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Front End Developer</h5>
-            </a>
-           
-          </div>
+
       </div>
-      <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+    </div>
+    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img src="">
+        <img class="rounded-t-lg p-8" src="https://source.unsplash.com/random/250%C3%97250/?parfume" alt="product image">
+      </a>
+      <div class="px-5 pb-5">
         <a href="#">
-          <img src="">
-            <img class="rounded-t-lg p-8" src="https://source.unsplash.com/random/250%C3%97250/?parfume" alt="product image">
+          <h3 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Moch Nizar Alfattan</h3>
+          <h5 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Front End Developer</h5>
         </a>
-          <div class="px-5 pb-5">
-            <a href="#">
-              <h3 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Moch Nizar Alfattan</h3>
-              <h5 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Front End Developer</h5>
-            </a>
-          </div>
-      </div>  
-  
+      </div>
+    </div>
+
     <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
         <img class="rounded-t-lg p-8  " src="https://source.unsplash.com/random/250%C3%97250/?parfume" alt="product image">
-          </a>
-        <div class="px-3 pb-3">
-          <a href="#">
+      </a>
+      <div class="px-3 pb-3">
+        <a href="#">
 
-            <h3 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Yogi Saputra M</h3>
+          <h3 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Yogi Saputra M</h3>
 
-            <h5 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Back End Developer</h5>
-          </a>
-        </div>
+          <h5 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Back End Developer</h5>
+        </a>
+      </div>
     </div>
-  
-    
-  
+
+
+
     <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700 ">
       <a href="#">
         <img class="rounded-t-lg p-8 " src="https://source.unsplash.com/random/250%C3%97250/?parfume" alt="product image">
-          </a>
-        <div class="px-5 pb-5">
-          <a href="#">
-            <h3 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Adan Ahmad Erlangga</h3>
-            <h5 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Back End Developer</h5>
-          </a>
-        </div>
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#">
+          <h3 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Adan Ahmad Erlangga</h3>
+          <h5 class="text-center text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Back End Developer</h5>
+        </a>
+      </div>
     </div>
   </div>
-  </div>
+</div>
 
 
 <h2 class="flex items-center justify-center mb-5 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Alamat Toko</h2>
 <div class="flex items-center justify-center mb-10 ">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.200537714376!2d107.59153091455858!3d-6.866556395037899!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e6be237133af%3A0x1637c744e3cdaab3!2sJl.%20Dr.%20Setiabudi%20No.193%2C%20Gegerkalong%2C%20Kec.%20Sukasari%2C%20Kota%20Bandung%2C%20Jawa%20Barat%2040153!5e0!3m2!1sid!2sid!4v1672573884626!5m2!1sid!2sid" width="750" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
-  </div>
-  
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.200537714376!2d107.59153091455858!3d-6.866556395037899!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e6be237133af%3A0x1637c744e3cdaab3!2sJl.%20Dr.%20Setiabudi%20No.193%2C%20Gegerkalong%2C%20Kec.%20Sukasari%2C%20Kota%20Bandung%2C%20Jawa%20Barat%2040153!5e0!3m2!1sid!2sid!4v1672573884626!5m2!1sid!2sid" width="750" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+</div>
+
 
 @endsection
-
- 
-
-  
